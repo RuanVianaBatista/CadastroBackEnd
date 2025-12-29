743829ca5672ae7440b90efe05afebaceb900b31
Morisl <ruanvianabatista@gmail.com>
2025-12-29 20:58:48 -0300

    Modelagem do banco de dados GuerreiroModel e MissaoModel concluidas, anotations feitas para relacionamento de tabelas.

--- Cadastro/src/main/java/Moris/Cadastro/guerreiro/GuerreiroModel.java
+++ Cadastro/src/main/java/Moris/Cadastro/guerreiro/GuerreiroModel.java
@@ -14,5 +14,67 @@ public class GuerreiroModel {
     private String nome;
     private String email;
     private int idade;
-    private List<MissaoModel> missao;
+
+    // @ManyToOne UM GUERREIRO TEM APENAS UMA UNICA MISSÃO.
+    @ManyToOne
+    @JoinColumn(name = "missao_id") // FORENG KEY
+    private MissaoModel missao;
+
+    public GuerreiroModel(String nome, String email, int idade, MissaoModel missao) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.missao = missao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public MissaoModel getMissao() {
+        return missao;
+    }
+
+    public void setMissao(MissaoModel missao) {
+        this.missao = missao;
+    }
+
+    @Override
+    public String toString() {
+        return "GuerreiroModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", idade=" + idade +
+                ", missao=" + missao +
+                '}';
+    }
 }
--- Cadastro/src/main/java/Moris/Cadastro/missao/MissaoModel.java
+++ Cadastro/src/main/java/Moris/Cadastro/missao/MissaoModel.java
@@ -11,5 +11,58 @@ public class MissaoModel {
     private long id;
     private String nome;
     private DificuldadeEnum dificuldade;
+
+    // @OneToMany UMA MISSÃO PODE TER VARIOS GUERREIROS
+    @OneToMany(mappedBy = "missao")
     private GuerreiroModel guerreiro;
+
+    public MissaoModel(String nome, DificuldadeEnum dificuldade, GuerreiroModel guerreiro) {
+        this.nome = nome;
+        this.dificuldade = dificuldade;
+        this.guerreiro = guerreiro;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public DificuldadeEnum getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(DificuldadeEnum dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public GuerreiroModel getGuerreiro() {
+        return guerreiro;
+    }
+
+    public void setGuerreiro(GuerreiroModel guerreiro) {
+        this.guerreiro = guerreiro;
+    }
+
+    @Override
+    public String toString() {
+        return "MissaoModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", dificuldade=" + dificuldade +
+                ", guerreiro=" + guerreiro +
+                '}';
+    }
 }
+
+
